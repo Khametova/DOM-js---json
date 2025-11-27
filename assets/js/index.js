ad2e3827e436fd5a3673c9b5bf4722a0f98178c7
Alina Khametova <alinakhametova1@gmail.com>
2025-11-27 14:49:42 +0200

    add change /js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,18 @@
 
 const dogImg_URL = "https://dog.ceo/api/breeds/image/random";
 
-fetch(dogImg_URL)
-  .then((response) => response.json())
-  .then((data) => updateImg(data));
+function getDog() {
+  fetch(dogImg_URL)
+    .then((response) => response.json())
+    .then((data) => updateImg(data));
+}
 
 function updateImg(imgData) {
   const imgDataEl = document.getElementById("dogImg");
   imgDataEl.src = imgData.message;
 }
+
+getDog();
+const btnEl = document.querySelector("btnImg");
+
+btnEl.onclick("click", getDog());
